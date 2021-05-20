3b123a3474ed197fc0de29ab3197d25d4df2003a
unknown <22guigs@gmail.com>
2021-05-20 13:32:09 -0400

    chore: all tests passed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("This user do not exists.");
+    }
+
+    if (!user.admin) {
+      throw new Error("This feature is only for admins.");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
